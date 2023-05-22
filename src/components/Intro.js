2002f5a1f03339c31e2ba7013e00365cbd9ad5a8
Sajid Iqbal <amawas1@gmail.com>
2023-05-22 20:24:58 +0500

    CV added

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ function Intro() {
         <h2>Hi</h2>
         <p>In the span of my web development journey, I have embarked on an exhilarating path of self-discovery and growth. As a passionate self-taught programmer, I have wholeheartedly embraced the captivating realm of web development, constantly seeking to expand my knowledge and master the art of crafting exceptional online experiences. Driven by my insatiable curiosity and love for coding, I have dived into an abundance of online resources, absorbing a multitude of languages, frameworks, and technologies that make up the dynamic web development landscape. Each day presents a fresh opportunity for me to explore new concepts, experiment with innovative techniques, and push the boundaries of my creativity. While the path has been challenging at times, my unwavering dedication and tenacity have propelled me forward, fueling my determination to excel in this ever-evolving field. As I continue to shape my skills and strive for excellence, I am invigorated by the boundless possibilities that lie ahead, eager to leave my mark on the digital world and contribute to the transformative power of web development.</p>
         <div className="button-wrapper">
-            <a href="#!" className="btn">Download Cv</a>
+        <a href="/Sajid%20Iqbal-JS-CV.pdf" download className="btn">Download CV</a>
         </div>
       </div>
     </section>
